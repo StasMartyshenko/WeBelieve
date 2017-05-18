030888896f6f6e284b815065b88d0a51b79db2fa
StasMartyshenko <martyshenkostas@gmail.com>
2017-05-19 00:08:44 +0300

    slSlider

--- script.js
+++ script.js
@@ -50,3 +50,38 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+
+//--------------------sl-slider------------------
+$('.sl').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 8,
+  slidesToScroll: 8,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
